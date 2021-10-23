1b88f57a974f6e5de751deacb96bbd637eed321c
ssun97 <sunmingyu.97@gmail.com>
2021-10-23 18:24:25 -0500

    update footer and header

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ window.onload = function() {
       url: "https://smileschool-api.hbtn.info/quotes",
       type: "GET",
       success: function(response) {
-          $("#loader").remove();
           response.forEach(function(quote, index) {
+              setTimeout(function() {
+                  $("#loader").remove();
                   let quoteHtml = `
                       <div class="carousel-item ${index === 0 ? "active" : ""} h-100">
                           <div class="carousel-card container text-white d-flex h-100 justify-content-center align-items-center d-flex flex-sm-row flex-column mt-5 mb-5">
@@ -19,7 +20,51 @@ window.onload = function() {
                       </div>
                   `;
                   $("#quotes").append(quoteHtml)
+              }, 1000);
           });
       }
-  })
+  });
+  // 2. Homepage - popular tutorials
+  function videos(url, id) {
+      $.ajax({
+      url: url,
+      type: "GET",
+      success: function(response) {
+          $("#loader").remove();
+          response.forEach(function(quote, index) {
+                  let quoteHtml = `
+                      <div class="card m-5" style="width: 16rem;">
+                          <div class=" d-flex align-items-center justify-content-center">
+                          <img src="${quote.thumb_url}" alt="" class="card-img-top">
+                          <img src="./images/play.png" alt="" class="position-absolute" style="width: 50px; height: 50px;">
+                          </div>
+                          <div class="card-body">
+                          <h5 class="card-title font-weight-bold text-left">${quote.title}</h5>
+                          <p class="card-text text-muted text-left">${quote['sub-title']}</p>
+                          <div class="d-flex align-items-center">
+                              <div class="mr-3">
+                              <img src="${quote.author_pic_url}" alt="" class="rounded-circle" style="width: 30px; height: 30px;">
+                              </div>
+                              <div class="carousel__user color-pink">
+                              ${quote.author}
+                              </div>
+                          </div>
+                          <div class="d-flex justify-content-between mt-3 align-items-center">
+                              <img src="./images/star_on.png" alt="" class="star" style="height: 20px; width: 20px;">
+                              <img src="./images/star_on.png" alt="" class="star" style="height: 20px; width: 20px;">
+                              <img src="./images/star_on.png" alt="" class="star" style="height: 20px; width: 20px;">
+                              <img src="./images/star_on.png" alt="" class="star" style="height: 20px; width: 20px;">
+                              <img src="./images/star_off.png" alt="" class="star" style="height: 20px; width: 20px;">
+                              <div class="color-pink carousel-time">${quote.duration}</div>
+                          </div>
+                          </div>
+                      </div>
+                  `;
+                  $(`#${id}`).append(quoteHtml)
+          });
+      }
+  });
+  }
+  videos("https://smileschool-api.hbtn.info/popular-tutorials", "pop-videos");
+  videos("https://smileschool-api.hbtn.info/latest-videos", "latest-videos");
 }
